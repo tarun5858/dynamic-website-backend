888c55631581e8685a36a06c2803c9b47b434f37
tarun5858 <ts460000@gmail.com>
2025-10-15 18:09:10 +0530

    latest commit

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import Blog from "./models/Blog.js";
+import dotenv from "dotenv";
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+dotenv.config();
 // app.use(cors());
 // app.use(cors({
 //   origin: "https://dynamic-website-react.onrender.com"
@@ -28,7 +30,6 @@ app.use(
     credentials: true,
   })
 );
-
 
 app.use(express.json());
 
