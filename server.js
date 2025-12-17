d0a3ed92e386406785343fb94f5d753eec310505
tarun5858 <ts460000@gmail.com>
2025-12-17 11:08:39 +0530

    update backend logic

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
   "https://demo.prehome.in",
   "https://dynamic-website-react.onrender.com",
   "http://localhost:5173",
+  "http://localhost:5174",
   "http://localhost:4173",
 ];
 
